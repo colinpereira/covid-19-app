c6fc08d1b7ca8e50104911813845c1b802b91d68
Shareef Halaby <shareeff99@gmail.com>
2022-05-02 19:53:11 +0300

    covid 19 app

---
+++ src/Routes/App.js
@@ -0,0 +1,12 @@
+
+import "./Style/App.css";
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>  /* End of the App */
+  );
+}
+
+export default App;
---
+++ src/Routes/Home.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { useEffect, useEffct } from 'react'
+import Axios from "axios" ;
+
+const Home = () => {
+  
+  const [country, setCountry] = useState([]);
+  const [searchTirm, setSearchTirm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Change the value of the setSearchTirm State
+  const handleSearch = (event) => {
+    setSearchTirm(event.target.value);
+  }
+
+  // when the page loead do something
+  useEffct(()=> {
+    refreshPage();
+  }, [])
+
+  const refreshPage = () => {
+    setIsLoading(true);
+      Axios.get(`https://covid-api.mmediagroup.fr/v1/cases?country=Israel`).then((response) => {
+          console.log(response.All);
+          setIsLoading(false);
+          setCountry(response.All);
+      })
+  }
+  
+    return (
+    <div className="App">
+
+        <div className="headerContainer">
+            <h1>Covid-19 Country Information</h1>
+            <button className="buttonContainer">
+                <input
+                type="text"
+                placeholder="Search for a country"
+                onChange={handleSearch}
+                />
+            </button>
+        </div>
+
+
+
+        <div className="covidInfo">
+
+        </div>
+
+    </div> // end of the App
+  )
+}
+
+export default Home
