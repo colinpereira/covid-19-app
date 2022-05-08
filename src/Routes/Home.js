13ec168eeac1baf078a3e52f6e3b1b801122833b
colinpereira <64594861+colinpereira@users.noreply.github.com>
2022-05-08 10:48:34 -0700

    issues fixed during class with colin - api call, useEffect

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,54 +1,46 @@
-import React, { useState } from 'react'
-import { useEffect, useEffct } from 'react'
-import Axios from "axios" ;
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 const Home = () => {
-  
-  const [country, setCountry] = useState([]);
+  const [country, setCountry] = useState({});
   const [searchTirm, setSearchTirm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   // Change the value of the setSearchTirm State
   const handleSearch = (event) => {
     setSearchTirm(event.target.value);
-  }
+  };
 
   // when the page loead do something
-  useEffct(()=> {
+  useEffect(() => {
     refreshPage();
-  }, [])
+  }, []);
 
   const refreshPage = () => {
     setIsLoading(true);
-      Axios.get(`https://covid-api.mmediagroup.fr/v1/cases?country=${All}`).then((response) => {
-          console.log(response.All);
-          setIsLoading(false);
-          setCountry(response.All);
-      })
-  }
-  
-    return (
+    Axios.get(`https://covid-api.mmediagroup.fr/v1/cases?`).then((response) => {
+      console.log(response.data);
+      setIsLoading(false);
+      setCountry(response.data);
+    });
+  };
+
+  return (
     <div className="App">
+      <div className="headerContainer">
+        <h1>Covid-19 Country Information</h1>
+        <button className="buttonContainer">
+          <input
+            type="text"
+            placeholder="Search for a country"
+            onChange={handleSearch}
+          />
+        </button>
+      </div>
 
-        <div className="headerContainer">
-            <h1>Covid-19 Country Information</h1>
-            <button className="buttonContainer">
-                <input
-                type="text"
-                placeholder="Search for a country"
-                onChange={handleSearch}
-                />
-            </button>
-        </div>
-
-
-
-        <div className="covidCountryInfo">
-
-        </div>
-
+      <div className="covidCountryInfo"></div>
     </div> // end of the App
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
